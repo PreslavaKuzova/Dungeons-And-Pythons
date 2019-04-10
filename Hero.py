975eb71311ee7a22cd56959d177e1d8d04eb86c3
alexa984 <alex.yovkova@gmail.com>
2019-04-10 12:47:19 +0300

    selfs

--- Hero.py
+++ Hero.py
@@ -10,45 +10,45 @@ class Hero:
         self.equiped = {} #set for weapons equiped
         self.spells = {} #set for known spells
 
-    def known_as():
+    def known_as(self):
         return "{} the {}".format(self.name, self.title)
 
-    def get_health():
+    def get_health(self):
         return self.health
 
-    def get_mana():
+    def get_mana(self):
         return self.mana
 
-    def is_alive():
+    def is_alive(self):
         if self.health > 0:
             return True
         else:
             return False
 
-    def can_cast():
+    def can_cast(self):
         if self.mana > 0:
             return True
         else:
             return False
 
-    def take_damage(dmg_points):
+    def take_damage(self, dmg_points):
         pass
 
-    def take_healing(healing_points):
+    def take_healing(self, healing_points):
         pass
 
-    def take_mana(mana_points):
+    def take_mana(self, mana_points):
         pass
 
-    def equip(weapon):
+    def equip(self, weapon):
         #should validate weapon to be instance of weapon class
         pass
 
-    def learn(spell):
+    def learn(self, spell):
         #should validate spell to be instance of Spell class
         pass
 
-    def attack(by=None):
+    def attack(self, by=None):
         pass
 
 
