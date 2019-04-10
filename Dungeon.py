2da83d990753403d077aafe5025dc38b5d89691e
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-04-10 16:06:11 +0300

    New spawn logic added

--- Dungeon.py
+++ Dungeon.py
@@ -1,40 +1,25 @@
 import os
+import tempfile
 from Hero import Hero
 
 class Dungeon:
     def __init__(self, map_directory):
         self.map_directory = map_directory
-        self.temp_map_directory = self._create_temp_map_directory()
-        self._fill_temp_map()
-    
-    def _create_temp_map_directory(self):
-        lst = self.map_directory.split('/')
-        lst = [x for x in lst if x != '' and x != '.']
-        lst[len(lst) - 1] = 'temp_' + lst[len(lst) - 1]
-        self.temp_map_directory = '/'.join(lst)
-        return self.temp_map_directory
-    
-    def _fill_temp_map(self):
-        with open(self.map_directory) as f:
-            with open(self.temp_map_directory, "w") as f1:
-                for line in f:
-                    f1.write(line)
+        self.tmp_map = []
+        self.fill_tmp_map()
+
+    def fill_tmp_map(self):
+        with open(self.map_directory, 'r') as f:
+            for line in f:
+                line = line.replace('\n', "")
+                self.tmp_map.append([x for x in line])
 
     def print_map(self):
-        with open(self.temp_map_directory, 'r') as f:
-            print(f.read())
+        for line in self.tmp_map:
+            print(''.join(line))
 
     def spawn(self, hero: Hero):
-        pass
-        # is_able_to_spawn = False
-        # tmp_file = open(self.create_temp_map_directory)
-        # with open(self.map_directory, 'r') as f:
-        #     for line in f:
-        #         for symbol in line:
-        #             if symbol == 'S':
-        #                 tmp_file.write('H')
-        #                 is_able_to_spawn = True
-        #             tmp_file.write(symbol)
-        #         tmp_file.write('\n')
-        # tmp_file.close()
-        # return is_able_to_spawn
+        for line in self.tmp_map:
+            for symbol in line:
+                if symbol == 'S':
+                    symbol = 'H'
