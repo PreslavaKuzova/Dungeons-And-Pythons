5a35cc465c160d0b17262a39bcf9577882b182ca
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-04-10 13:07:36 +0300

    Add Spell and Weapon layou

---
+++ Spell.py
@@ -0,0 +1,6 @@
+class Spell:
+    def __init__(self, name, damage, mana_cost, cast_range):
+        self.name = name
+        self.damage = damage
+        self.mana_cost = mana_cost
+        self.cast_range = cast_range
---
+++ Weapon.py
@@ -0,0 +1,4 @@
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
