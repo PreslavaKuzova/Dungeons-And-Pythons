7fee09619744bba8d973746eadd9d70ff9dc5fb3
alexa984 <alex.yovkova@gmail.com>
2019-04-10 18:20:35 +0300

    added __eq__

--- Spell.py
+++ Spell.py
@@ -4,3 +4,9 @@ class Spell:
         self.damage = damage
         self.mana_cost = mana_cost
         self.cast_range = cast_range
+
+    def __eq__(self, other):
+        for k in self.__dict__.keys():
+            if self.__dict__[k] != other.__dict__[k]:
+                return False
+        return True
--- Weapon.py
+++ Weapon.py
@@ -2,3 +2,9 @@ class Weapon:
     def __init__(self, name, damage):
         self.name = name
         self.damage = damage
+
+    def __eq__(self, other):
+        for k in self.__dict__.keys():
+            if self.__dict__[k] != other.__dict__[k]:
+                return False
+        return True
