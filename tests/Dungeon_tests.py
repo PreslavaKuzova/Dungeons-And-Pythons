3bd1a051f61b618f9cef30efb8d72f66a80cbf6a
PreslavaKuzova <preslava.kuzova@gmail.com>
2019-04-15 11:49:00 +0300

    Adding initial Dungeon tests

--- tests/Dungeon_tests.py
+++ tests/Dungeon_tests.py
@@ -2,12 +2,37 @@ import unittest
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Dungeon import *
+from Hero import *
+
 
 class TestDungeon(unittest.TestCase):
+    def test_dungeon_initialization_when_empty_map(self):
+        h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
+        d = Dungeon('test_maps/empty.txt', h, 2)
+        self.assertEqual(d.tmp_map, [])
+    
+    def test_dungeon_initialization_when_map_is_correct(self):
+        h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
+        d = Dungeon('test_maps/test.txt', h, 2)
+        expected_map = [['.', '.', '#'],['S', '.', '.'],['#', '.', 'G']]
+        self.assertEqual(d.tmp_map, expected_map)
+    
+    def test_spawning_the_hero_on_the_S_location_on_map(self):
+        h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
+        d = Dungeon('test_maps/test.txt', h, 2)
+        d.spawn()
+        result = [d.hero_position_X, d.hero_position_Y]
+        expected = [1, 0]
+        self.assertEqual(result, expected)
+    
+    def test_getting_the_end_point_of_the_map_then_return_its_coordinates(self):
+        h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
+        d = Dungeon('test_maps/test.txt', h, 2)
+        result = d.endpoint
+        expected = (2, 2)
+        self.assertEqual(result, expected)
 
-    def test_something(self):
-        pass
-        
-        
+    
+
 if __name__ =='__main__':
     unittest.main()
